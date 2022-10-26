14f3e2a12554070635a34e9efdce41dacf9a7abf
kaiwen <kwwu2015@gmail.com>
2022-10-26 16:46:44 -0400

    fix a bug; add a test case

--- src/opt/levenberg_marquardt.py
+++ src/opt/levenberg_marquardt.py
@@ -7,8 +7,7 @@ from residual import ResidualFunctional
 
 
 def concatenate(u, lengthscale):
-    inputs = u.view(-1)
-    inputs = torch.cat((inputs, lengthscale), dim=-1)
+    inputs = torch.cat((u.view(-1), lengthscale.view(-1)), dim=-1)
     return inputs
 
 
@@ -16,15 +15,16 @@ def extract(inputs, m, d):
     assert inputs.size(0) == m * d + 1
 
     u = inputs[:m * d].view(m, d)
-    lengthscale = torch.nn.functional.softplus(inputs[-1])
+    lengthscale = torch.nn.functional.softplus(inputs[-1]).view(1, 1)
 
     return u, lengthscale
 
 
-@torch.no_grad
+@torch.no_grad()
 def levenberg_marquardt(
     u, x, y, kernel, sigma,
-    nsteps=100, rtol=1e-8, tau=1e-3):
+    nsteps=100, rtol=1e-8, tau=1e-3
+):
     """
     Args
         u (m x d tensor): initialization of inducing points
@@ -73,7 +73,7 @@ def levenberg_marquardt(
             inputs = updated_inputs
             residual = updated_residual
 
-            jacobian = functional.jacobian(u, x, y)
+            jacobian = functional.jacobian(inputs, x, y)
             hessian = jacobian.T @ jacobian
 
             # Reset re-scaling parameter, update damping
@@ -87,61 +87,19 @@ def levenberg_marquardt(
     return extract(inputs, m, d), norm_hist
 
 
-def _levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3):
+if __name__ == "__main__":
+    import gpytorch
 
-    # Evaluate everything at the initial point
-    x = torch.clone(x)
-    xnew = torch.clone(x)
-    theta_new = theta
-    fx = f(x,theta)
-    Jx = J(x,theta)
-    Hx = torch.matmul(Jx.T, Jx)
-    
-#     print(f"x = {x}, Hx = {Hx}")
-    mu = tau * max(torch.diag(Hx)).item()
-    v = 2.0
-    norm_hist = []
-    for k in range(nsteps):
-        g = torch.matmul(Jx.T, fx)
-        rnorm = torch.norm(g)
-        # print(f"k: {k+1}, rnorm: {rnorm}, mu={mu}, v={v}")
-        norm_hist.append(rnorm.item())
-        if rnorm < rtol:
-            return x, theta
-        
-        # Compute a proposed step and re-evaluate residual vector
-        D = torch.diag(mu*torch.ones(Hx.shape[0])).to(device=Hx.device)
-        p = torch.linalg.solve((Hx + D), -g)
-        
-        xnew_flatten = x.flatten() + p[:-1]
-        xnew = xnew_flatten.reshape(x.shape)
-        theta_new = theta + p[-1].item()
-        fxnew = f(xnew, theta_new)
-        
-        # Compute the gain ratio
-        rho = (torch.norm(fx)**2 - torch.norm(fxnew)**2) / (torch.norm(fx)**2 - torch.norm(fx+torch.matmul(Jx,p))**2)
-        if rho > 0:  # Success!
-            # print(f"success, theta_new: {theta_new}")
-            # Accept new point
-            # print("delete fx, Jx, Hx.")
-            # sys.stdout.flush()
-            # check_cuda_memory()
-            del fx, Jx, Hx
-            x = xnew
-            theta = theta_new
-            fx = fxnew
-            del fxnew, xnew
-            Jx = J(x,theta)
-            Hx = torch.matmul(Jx.T, Jx)
-            # check_cuda_memory()
-            # Reset re-scaling parameter, update damping
-            mu = mu*max(1.0/3.0, 1.0-2.0*(rho.item()-1.0)**3)
-            v = 2.0
-        else:
-            # Rescale damping5tb
-            mu = mu*v
-            v = 2*v
-    
-    return x, theta, norm_hist
-    
- 
+    n = 5
+    d = 2
+    m = 3
+
+    torch.manual_seed(0)
+    u = torch.randn(m, d)
+    x = torch.randn(n, d)
+    y = torch.randn(n)
+
+    kernel = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+    (inducing_points, lengthscale), norm_hist = levenberg_marquardt(u, x, y, kernel, sigma=1e-2)
+    print(norm_hist)
