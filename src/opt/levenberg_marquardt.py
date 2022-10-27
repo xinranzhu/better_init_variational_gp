04977957fa5faaf2c1467d632a5e173761a0fd9e
Xinran Zhu <xz584@cornell.edu>
2022-10-27 13:08:15 -0400

    add log to LM

--- src/opt/levenberg_marquardt.py
+++ src/opt/levenberg_marquardt.py
@@ -3,9 +3,10 @@ import torch
 sys.path.append("../")
 from utils import check_cuda_memory
 
-def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3):
+def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3, output_steps=None, log_each_step=False):
 
     # Evaluate everything at the initial point
+    m, dim = x.shape
     x = torch.clone(x)
     xnew = torch.clone(x)
     theta_new = theta
@@ -17,6 +18,17 @@ def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3):
     mu = tau * max(torch.diag(Hx)).item()
     v = 2.0
     norm_hist = []
+
+    res_dict = {}
+    if output_steps is not None:
+        for step in output_steps:
+            res_dict[step] = {}
+
+    log_dict = {"u": torch.zeros(nsteps, m, dim),
+                "grad_norm": torch.zeros(nsteps),
+                "theta": torch.zeros(nsteps), 
+                "training_rmse": torch.zeros(nsteps),
+                }
     for k in range(nsteps):
         g = torch.matmul(Jx.T, fx)
         rnorm = torch.norm(g)
@@ -57,7 +69,22 @@ def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3):
             # Rescale damping5tb
             mu = mu*v
             v = 2*v
-    
-    return x, theta, norm_hist
+
+        if output_steps is not None and k in output_steps:
+            print(k)
+            sys.stdout.flush()
+            res_dict[k] = {"u": x.cpu(), "theta": theta,
+                           "train_rmse": (fx.cpu()**2).mean().sqrt()}
+
+        if log_each_step:
+            log_dict["u"][k] = x.cpu()
+            log_dict["grad_norm"][k] = rnorm.cpu()
+            log_dict["theta"][k] = theta
+            log_dict["training_rmse"][k] = (fx.cpu()**2).mean().sqrt()
+
+    if log_each_step:
+        return x, theta, norm_hist, res_dict, (fx.cpu()**2).mean().sqrt(), log_dict
+    else:
+        return x, theta, norm_hist, res_dict, (fx.cpu()**2).mean().sqrt()
     
  
