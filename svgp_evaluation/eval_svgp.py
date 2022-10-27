3627893f3c07d9e86a4a381a8f3f66ed44f90444
Xinran Zhu <xz584@cornell.edu>
2022-10-27 13:11:00 -0400

    minor modifications

--- svgp_evaluation/eval_svgp.py
+++ svgp_evaluation/eval_svgp.py
@@ -7,7 +7,7 @@ import torch
 import gpytorch
 import pickle as pkl
 sys.path.append("./models")
-from svgp import GPModel, get_inducing_points, train_gp, eval_gp
+from svgp import GPModel, get_inducing_points, train_gp, eval_gp, _select_inducing_points
 from svgp_utils import meshgrid_uniform
 from eval_experiment import Experiment
 
@@ -34,7 +34,11 @@ class SVGP_exp(Experiment):
         learn_m=True,
         use_ngd=False, ngd_lr=0.1,
         save_model=False,
-        init_covar=True, 
+        init_theta=True,
+        init_noise=True,
+        init_covar=True,
+        init_mean=True, 
+        lm_step=None,
         ):
 
         self.method_args['init_hypers'] = locals()
@@ -55,44 +59,74 @@ class SVGP_exp(Experiment):
             kmeans = KMeans(n_clusters=num_inducing, random_state=0).fit(xk)
             u0 = kmeans.cluster_centers_
         else: # method = "fwd", "lm" or "tr_newton"
-            res = pkl.load(open(f"../results/{self.obj_name}-{self.dim}_{init_method}_m{m}_{init_expid}_{self.seed}.pkl", "rb"))
-            u0 = res["u"]
+            if init_method == "lm_init":
+                res = pkl.load(open(f"../results/{self.obj_name}-{self.dim}_kmeans_m{m}_{init_expid}_{self.seed}.pkl", "rb"))
+            else:
+                if lm_step is None:
+                    res = pkl.load(open(f"../results/{self.obj_name}-{self.dim}_{init_method}_m{m}_{init_expid}_{self.seed}.pkl", "rb"))
+                else:
+                    res = pkl.load(open(f"../results/{self.obj_name}-{self.dim}_{init_method}_m{m}_{init_expid}_{self.seed}_step{lm_step}.pkl", "rb"))
+            u0 = res["u"].to(device=self.device)
             c = res["c"]
             sigma = res["sigma"]
             theta = res["theta"]
             time_cost = res["time"]
             if init_covar:
                 Sbar = res["Sbar"]
-                Lbar = torch.linalg.cholesky(Sbar)
+                try:
+                    Lbar = torch.linalg.cholesky(Sbar)
+                except: # failed to initialize variational covariance
+                    init_covar=False 
             print(f"Pretraining by {init_method} cost: {time_cost} sec.")
             assert u0.shape[0] == num_inducing and u0.shape[1] == self.dim
-
+        
         u0 = torch.tensor(u0)
         model = GPModel(inducing_points=u0, 
-            learn_inducing_locations=learn_u,
-            use_ngd=use_ngd)
+                learn_inducing_locations=learn_u,
+                use_ngd=use_ngd)
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        
+        if init_method == "pivchol":
+            # compute pivoted cholesky initialization for inducing points
+            input_batch_shape = self.train_x.shape[:-2]
+            u0_new = _select_inducing_points(
+                self.train_x,
+                model.covar_module,
+                num_inducing,
+                input_batch_shape,
+            )
+            print("norm difference between u0 and u0_new: ", torch.norm(u0-u0_new))
+            model = GPModel(inducing_points=u0_new, 
+                learn_inducing_locations=learn_u,
+                use_ngd=use_ngd)
 
-        if init_method not in {"random", "kmeans", "random_init_noise"}:
-            print("Initializing noise, theta and variational_mean")
-            hypers = {
-                    'likelihood.noise_covar.noise': torch.tensor(sigma**2),
-                    'covar_module.lengthscale': torch.tensor(theta),
-                    }
+        if init_method not in {"random", "kmeans", "random_init_noise", "pivchol"}:
+            hypers = {}
+            hypers_likelihood = {}
             if use_ngd:
                 hypers["variational_strategy._variational_distribution.natural_vec"] = c.to(u0.device)
             else:
+                if init_theta:
+                    print("initializing theta.")
+                    hypers['covar_module.lengthscale'] =  torch.tensor(theta)
+                if init_noise: 
+                    print("initializing noise.")
+                    hypers_likelihood["likelihood.noise_covar.noise"] = torch.tensor(sigma**2)
                 if init_covar:
                     print("initializing covar.")
                     hypers["variational_strategy._variational_distribution.chol_variational_covar"] = Lbar.to(u0.device)
-                hypers["variational_strategy._variational_distribution.variational_mean"] = c.to(u0.device)
+                    model.variational_strategy.variational_covar_initialized = torch.tensor(1)
+                if init_mean:
+                    print("initializing mean.")
+                    hypers["variational_strategy._variational_distribution.variational_mean"] = c.to(u0.device)
+                    model.variational_strategy.variational_mean_initialized = torch.tensor(1)
             model.initialize(**hypers)
+            likelihood.initialize(**hypers_likelihood)
         if init_method == "random_init_noise":
-            hypers = {
-                    'likelihood.noise_covar.noise': torch.tensor(0.1**2),
-                    }  
-            model.initialize(**hypers)
+            hypers_likelihood = {'likelihood.noise_covar.noise': torch.tensor(0.1**2)}  
+            likelihood.initialize(**hypers_likelihood)
         self.model = model
-        self.likelihood = model.likelihood
+        self.likelihood = likelihood
         self.use_ngd = use_ngd
         self.ngd_lr = ngd_lr
         self.save_model = save_model
@@ -122,6 +156,7 @@ class SVGP_exp(Experiment):
             self.test_x, self.test_y, 
             device=self.device,
             tracker=None)
+        print(f"initial test rmse: {rmse:.4e}, test nll: {test_nll:.4e}")
         
         self.model, self.likelihood, _, = train_gp(
             self.model, self.likelihood, 
--- svgp_evaluation/models/svgp.py
+++ svgp_evaluation/models/svgp.py
@@ -8,25 +8,150 @@ from gpytorch.variational import CholeskyVariationalDistribution, NaturalVariati
 from gpytorch.variational import VariationalStrategy
 from torch.utils.data import TensorDataset, DataLoader
 
+def _pivoted_cholesky_init(
+    train_inputs,
+    kernel_matrix,
+    max_length,
+    epsilon=1e-6,
+):
+    r"""
+    A pivoted cholesky initialization method for the inducing points,
+    originally proposed in [burt2020svgp]_ with the algorithm itself coming from
+    [chen2018dpp]_. Code is a PyTorch version from [chen2018dpp]_, copied from
+    https://github.com/laming-chen/fast-map-dpp/blob/master/dpp.py.
+
+    Args:
+        train_inputs: training inputs (of shape n x d)
+        kernel_matrix: kernel matrix on the training
+            inputs
+        max_length: number of inducing points to initialize
+        epsilon: numerical jitter for stability.
+
+    Returns:
+        max_length x d tensor of the training inputs corresponding to the top
+        max_length pivots of the training kernel matrix
+    """
+    # this is numerically equivalent to iteratively performing a pivoted cholesky
+    # while storing the diagonal pivots at each iteration
+    # TODO: use gpytorch's pivoted cholesky instead once that gets an exposed list
+    # TODO: ensure this works in batch mode, which it does not currently.
+    NEG_INF = -(torch.tensor(float("inf")))
+    item_size = kernel_matrix.shape[-2]
+    cis = torch.zeros(
+        (max_length, item_size), device=kernel_matrix.device, dtype=kernel_matrix.dtype
+    )
+    di2s = kernel_matrix.diag()
+    selected_items = []
+    selected_item = torch.argmax(di2s)
+    selected_items.append(selected_item)
+
+    while len(selected_items) < max_length:
+        k = len(selected_items) - 1
+        ci_optimal = cis[:k, selected_item]
+        di_optimal = torch.sqrt(di2s[selected_item])
+        elements = kernel_matrix[..., selected_item, :]
+        eis = (elements - torch.matmul(ci_optimal, cis[:k, :])) / di_optimal
+        cis[k, :] = eis
+        di2s = di2s - eis.pow(2.0)
+        di2s[selected_item] = NEG_INF
+        selected_item = torch.argmax(di2s)
+        if di2s[selected_item] < epsilon:
+            break
+        selected_items.append(selected_item)
+
+    ind_points = train_inputs[torch.stack(selected_items)]
+
+    return ind_points
+
+def _select_inducing_points(
+    inputs,
+    covar_module,
+    num_inducing,
+    input_batch_shape,
+):
+    r"""
+    Utility function that evaluates a kernel at given inputs and selects inducing point
+    locations based on the pivoted Cholesky heuristic.
+
+    Args:
+        inputs: A (*batch_shape, n, d)-dim input data tensor.
+        covar_module: GPyTorch Module returning a LazyTensor kernel matrix.
+        num_inducing: The maximun number (m) of inducing points (m <= n).
+        input_batch_shape: The non-task-related batch shape.
+
+    Returns:
+        A (*batch_shape, m, d)-dim tensor of inducing point locations.
+    """
+
+    train_train_kernel = covar_module(inputs).evaluate_kernel()
+
+    # base case
+    if train_train_kernel.ndimension() == 2:
+        inducing_points = _pivoted_cholesky_init(
+            train_inputs=inputs,
+            kernel_matrix=train_train_kernel,
+            max_length=num_inducing,
+        )
+    # multi-task case
+    elif train_train_kernel.ndimension() == 3 and len(input_batch_shape) == 0:
+        input_element = inputs[0] if inputs.ndimension() == 3 else inputs
+        kernel_element = train_train_kernel[0]
+        inducing_points = _pivoted_cholesky_init(
+            train_inputs=input_element,
+            kernel_matrix=kernel_element,
+            max_length=num_inducing,
+        )
+    # batched input cases
+    else:
+        batched_inputs = (
+            inputs.expand(*input_batch_shape, -1, -1)
+            if inputs.ndimension() == 2
+            else inputs
+        )
+        reshaped_inputs = batched_inputs.flatten(end_dim=-3)
+        inducing_points = []
+        for input_element in reshaped_inputs:
+            # the extra kernel evals are a little wasteful but make it
+            # easier to infer the task batch size
+            kernel_element = covar_module(input_element).evaluate_kernel()
+            # handle extra task batch dimension
+            kernel_element = (
+                kernel_element[0]
+                if kernel_element.ndimension() == 3
+                else kernel_element
+            )
+            inducing_points.append(
+                _pivoted_cholesky_init(
+                    train_inputs=input_element,
+                    kernel_matrix=kernel_element,
+                    max_length=num_inducing,
+                )
+            )
+        inducing_points = torch.stack(inducing_points).view(
+            *input_batch_shape, num_inducing, -1
+        )
+
+    return inducing_points
+
 class GPModel(ApproximateGP):
     def __init__(self, inducing_points, kernel_type='se', 
         learn_inducing_locations=True, 
-        use_ngd=False,
+        use_ngd=False, 
         ):
+        
         if use_ngd:
             variational_distribution = NaturalVariationalDistribution(inducing_points.size(0))
         else:
             variational_distribution = CholeskyVariationalDistribution(inducing_points.size(0))
 
-        # variational_strategy = UnwhitenedVariationalStrategy(self, inducing_points, 
-        #                                            variational_distribution, 
-        #                                            learn_inducing_locations=learn_inducing_locations)
         variational_strategy = VariationalStrategy(self, inducing_points, 
                                                    variational_distribution, 
                                                    learn_inducing_locations=learn_inducing_locations)
         super(GPModel, self).__init__(variational_strategy)
-        self.mean_module = gpytorch.means.ConstantMean()
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        
+        self.mean_module = gpytorch.means.ConstantMean()
+        # self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
         # self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
         # XZ: remove the outside scaling factor for now
         if kernel_type == 'se':
@@ -37,8 +162,7 @@ class GPModel(ApproximateGP):
             self.covar_module = gpytorch.kernels.MaternKernel(nu=1.5)
         elif kernel_type == 'matern5/2':
             self.covar_module = gpytorch.kernels.MaternKernel(nu=2.5)
-            
-
+         
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
@@ -68,11 +192,12 @@ def train_gp(model, likelihood, train_x, train_y,
     test_x=None, test_y=None,
     val_x=None, val_y=None,
     load_run_path=None,
-    learn_S_only=False,
     separate_group=None, lr2=None, gamma2=None,
-    learn_variational_only=False, learn_hyper_only=False,
+    learn_S_only=False, learn_variational_only=False, learn_hyper_only=False,
     ):
     gamma2 = gamma if gamma2 is None else gamma2
+    lr2 = lr if lr2 is None else lr2
+
 
     train_dataset = TensorDataset(train_x, train_y)
     train_loader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
@@ -87,9 +212,6 @@ def train_gp(model, likelihood, train_x, train_y,
     if device == "cuda":
         model = model.to(device=torch.device("cuda"))
         likelihood = likelihood.to(device=torch.device("cuda"))
-    
-    model.train()
-    likelihood.train()
 
     if use_ngd:
         print("using NGD")
@@ -101,8 +223,10 @@ def train_gp(model, likelihood, train_x, train_y,
         if learn_inducing_values:
             print("normal learning")
             optimizer1 = torch.optim.Adam([
-                {'params': model.hyperparameters()}, # inducing points, mean_const, raw_noise, raw_lengthscale 
+                {'params': model.hyperparameters()},
+                {'params': likelihood.parameters()} # inducing points, mean_const, raw_noise, raw_lengthscale 
             ], lr=lr)
+
             optimizer2 =  torch.optim.Adam([
                 {'params': model.variational_parameters()}, 
             ], lr=lr)
@@ -117,16 +241,18 @@ def train_gp(model, likelihood, train_x, train_y,
             ], lr=lr)
 
     if separate_group == "u_m_covar":
+        print("u_m_covar")
         optimizer1 =  torch.optim.Adam([
                 {'params': model.covar_module.raw_lengthscale}, 
                 {'params':  model.mean_module.constant},
-                {'params': model.likelihood.raw_noise},
+                {'params': likelihood.raw_noise},
             ], lr=lr) 
         optimizer2 =  torch.optim.Adam([
                 {'params': model.variational_parameters()},
                 {'params': model.variational_strategy.inducing_points},  
             ], lr=lr2)
     elif separate_group == "u":
+        print("u")
         optimizer2 =  torch.optim.Adam([
                 {'params': model.variational_strategy.inducing_points}, 
             ], lr=lr2)
@@ -134,9 +260,10 @@ def train_gp(model, likelihood, train_x, train_y,
                 {'params': model.variational_parameters()},
                 {'params': model.covar_module.raw_lengthscale},
                  {'params':  model.mean_module.constant}, 
-                 {'params': model.likelihood.raw_noise},
+                 {'params': likelihood.raw_noise},
             ], lr=lr)
     elif separate_group == "u_m":
+        print("u_m")
         optimizer2 =  torch.optim.Adam([
                 {'params': model.variational_strategy.inducing_points}, 
                 {'params': model.variational_strategy._variational_distribution.variational_mean}, 
@@ -145,7 +272,7 @@ def train_gp(model, likelihood, train_x, train_y,
                 {'params': model.variational_strategy._variational_distribution.chol_variational_covar},
                 {'params': model.covar_module.raw_lengthscale},
                  {'params':  model.mean_module.constant}, 
-                 {'params': model.likelihood.raw_noise},
+                 {'params': likelihood.raw_noise},
             ], lr=lr)
 
     if learn_S_only:
@@ -166,12 +293,12 @@ def train_gp(model, likelihood, train_x, train_y,
         optimizer1 =  torch.optim.Adam([
                 {'params': model.covar_module.raw_lengthscale}, 
                 {'params':  model.mean_module.constant},
-                {'params': model.likelihood.raw_noise},
+                {'params': likelihood.raw_noise},
             ], lr=lr) 
         optimizer2 = None
 
-    # check_optimizer(optimizer1, name="optimizer1")
-    # check_optimizer(optimizer2, name="optimizer2")
+    check_optimizer(optimizer1, name="optimizer1")
+    check_optimizer(optimizer2, name="optimizer2")
 
     if scheduler == "multistep":
         milestones = [int(num_epochs*len(train_loader)/3), int(2*num_epochs*len(train_loader)/3)]
@@ -204,9 +331,12 @@ def train_gp(model, likelihood, train_x, train_y,
     start = time.time()
     min_val_rmse = float("Inf")
     min_val_nll = float("Inf")
+    model.train()
+    likelihood.train()
+
     for i in range(num_epochs-previous_epoch):
         # time1 = time.time()
-        for x_batch, y_batch in train_loader:
+        for k, (x_batch, y_batch) in enumerate(train_loader):
             if device == "cuda":
                 x_batch = x_batch.cuda()
                 y_batch = y_batch.cuda()
@@ -215,6 +345,7 @@ def train_gp(model, likelihood, train_x, train_y,
             if optimizer2 is not None:
                 optimizer2.zero_grad()
             output = likelihood(model(x_batch))
+            # output = model(x_batch)
             loss = -mll(output, y_batch)
             loss.backward()
             if optimizer1 is not None:
@@ -223,15 +354,6 @@ def train_gp(model, likelihood, train_x, train_y,
             if optimizer2 is not None:
                 optimizer2.step()
                 scheduler2.step()
-
-        if i == 0:
-            # make sure 
-            print("u.grad, ", model.variational_strategy.inducing_points.grad.abs().mean().item())
-            print("covar.grad, ", model.variational_strategy._variational_distribution.chol_variational_covar.grad.abs().mean().item())
-            print("mean.grad, ", model.variational_strategy._variational_distribution.variational_mean.grad.abs().mean().item())
-            print("raw_lengthscale.grad, ", model.covar_module.raw_lengthscale.grad.abs().mean().item())
-            print("mean_const.grad, ", model.mean_module.constant.grad.abs().mean().item())
-            print("raw_noise.grad, ", model.likelihood.raw_noise.grad.abs().mean().item())
 
         means = output.mean.cpu()
         stds  = output.variance.sqrt().cpu()
@@ -244,20 +366,29 @@ def train_gp(model, likelihood, train_x, train_y,
                 "training_nll": nll,    
             }, step=i+previous_epoch)
         if i % 10 == 0:
+            print(f"\n\nEpoch {i}, loss: {loss.item():.3f}, nll: {nll:.3f}, rmse: {rmse:.3e}")
+            print("u.grad, ", model.variational_strategy.inducing_points.grad.abs().mean().item())
+            print("covar.grad, ", model.variational_strategy._variational_distribution.chol_variational_covar.grad.abs().mean().item())
+            print("mean.grad, ", model.variational_strategy._variational_distribution.variational_mean.grad.abs().mean().item())
+            print("raw_lengthscale.grad, ", model.covar_module.raw_lengthscale.grad.abs().mean().item())
+            print("mean_const.grad, ", model.mean_module.constant.grad.abs().mean().item())
+            print("raw_noise.grad, ", likelihood.raw_noise.grad.abs().mean().item())
+
             min_val_rmse, min_val_nll = val_gp(model, likelihood, val_x, val_y,
                 test_batch_size=1024, device=device, tracker=tracker, step=i+previous_epoch, 
                 min_val_rmse=min_val_rmse, min_val_nll=min_val_nll
                 )
-            _, _, _, _, _ = eval_gp(model, likelihood, test_x, test_y,
+            _, _, test_rmse, test_nll, _ = eval_gp(model, likelihood, test_x, test_y,
                 test_batch_size=1024, device=device, tracker=tracker, step=i+previous_epoch)
+            print(f"testing rmse: {test_rmse:.3e}, nll:{test_nll:.3f}.")
             model.train()
             likelihood.train()
-            print(f"Epoch: {i}, loss: {loss.item()}, nll: {nll}, rmse: {rmse}")
+            # print(f"Epoch: {i}, loss: {loss.item()}, nll: {nll}, rmse: {rmse}")
             # print("optimizer1: lr = ", optimizer1.param_groups[0]['lr'])
             # print("optimizer2: lr = ", optimizer2.param_groups[0]['lr'])
             sys.stdout.flush()
             if save_model:
-                state = {"model": model.state_dict(), "epoch": i}
+                state = {"model": model.state_dict(), "likelihood": likelihood, "epoch": i}
                 torch.save(state, f'{save_path}.model')
         # time2 = time.time()
         # print("Time cost for 1 epoch: ", time2-time1)
@@ -268,8 +399,10 @@ def train_gp(model, likelihood, train_x, train_y,
                 test_batch_size=1024, device=device, tracker=tracker, step=i+previous_epoch, 
                 min_val_rmse=min_val_rmse, min_val_nll=min_val_nll
                 )
-    _, _, _, _, _ = eval_gp(model, likelihood, test_x, test_y,
+    _, _, test_rmse, test_nll, _ = eval_gp(model, likelihood, test_x, test_y,
         test_batch_size=1024, device=device, tracker=tracker, step=i+previous_epoch)
+    print(f"\nLast testing rmse: {test_rmse:.3e}, nll:{test_nll:.3f}.")
+
     sys.stdout.flush()
     if save_model:
         state = {"model": model.state_dict(), "epoch": i}
@@ -350,6 +483,7 @@ def val_gp(model, likelihood, val_x, val_y,
             if device == "cuda":
                 x_batch = x_batch.cuda()
             preds = likelihood(model(x_batch))
+            # preds = model(x_batch)
             if device == "cuda":
                 means = torch.cat([means, preds.mean.cpu()])
                 variances = torch.cat([variances, preds.variance.cpu()])
