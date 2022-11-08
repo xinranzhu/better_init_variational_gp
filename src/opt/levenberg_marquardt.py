264fea8b58b72995005227106208681db8e321f0
kaiwen <kwwu2015@gmail.com>
2022-11-08 14:01:08 -0500

    speed up the implementation (buggy)

--- src/opt/levenberg_marquardt.py
+++ src/opt/levenberg_marquardt.py
@@ -1,21 +1,53 @@
-import sys
 import torch 
+
+import sys
 sys.path.append("../")
-from utils import check_cuda_memory
 
-def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3, output_steps=None, log_each_step=False):
+from kernels import SEKernel
+from residual import lstsq_residual, lstsq_jacobian
 
-    # Evaluate everything at the initial point
-    m, dim = x.shape
-    x = torch.clone(x)
-    xnew = torch.clone(x)
-    theta_new = theta
-    fx = f(x,theta)
-    Jx = J(x,theta)
-    Hx = torch.matmul(Jx.T, Jx)
-    
-#     print(f"x = {x}, Hx = {Hx}")
-    mu = tau * max(torch.diag(Hx)).item()
+
+def phi(rho, theta):
+    kernel = SEKernel(theta=theta)
+    return kernel.phi(rho)
+
+
+def Drho_phi(rho, theta):
+    kernel = SEKernel(theta=theta)
+    return kernel.Drho_phi(rho)
+
+
+def Dtheta_phi(rho, theta):
+    kernel = SEKernel(theta=theta)
+    return kernel.Dtheta_phi(rho)
+
+
+def levenberg_marquardt(
+    u, x, y, theta, theta_penalty,
+    nsteps=100, rtol=1e-8, tau=1e-3, output_steps=None, log_each_step=False
+):
+    m, d = u.shape
+
+    def func_residual(u, theta):
+        residual, c, Q, R = lstsq_residual(u, x, y, theta, phi)
+        # return torch.cat([residual, theta_penalty * theta * torch.ones(1, device=u.device)]), c, Q, R
+        return residual, c, Q, R
+
+    def func_jacobian(u, theta, r, c, Q, R):
+        jacobian = lstsq_jacobian(u, x, y, theta, Drho_phi, Dtheta_phi, r, c, Q, R)
+        return jacobian
+        # t = torch.zeros(jacobian.shape[1]).to(u.device)
+        # t[-1] = theta_penalty
+        # return torch.cat([jacobian, t.reshape(1, -1)], dim=0)
+
+    residual, c, Q, R = func_residual(u, theta)
+    jacobian = func_jacobian(u, theta, residual, c, Q, R)
+    hessian = jacobian.T @ jacobian
+
+    # no need to compute hessian explicitly, which is slightly faster
+    # mu = tau * jacobian.square().sum(dim=-1).max()
+    mu = tau * torch.diag(hessian).max()
+
     v = 2.0
     norm_hist = []
 
@@ -24,67 +56,85 @@ def levenberg_marquardt(f, J, x, theta, nsteps=100, rtol=1e-8, tau=1e-3, output_
         for step in output_steps:
             res_dict[step] = {}
 
-    log_dict = {"u": torch.zeros(nsteps, m, dim),
-                "grad_norm": torch.zeros(nsteps),
-                "theta": torch.zeros(nsteps), 
-                "training_rmse": torch.zeros(nsteps),
-                }
+    log_dict = {
+        "u": torch.zeros(nsteps, m, d),
+        "grad_norm": torch.zeros(nsteps),
+        "theta": torch.zeros(nsteps),
+        "training_rmse": torch.zeros(nsteps),
+    }
+
     for k in range(nsteps):
-        g = torch.matmul(Jx.T, fx)
+        g = jacobian.T @ residual
         rnorm = torch.norm(g)
-        # print(f"k: {k+1}, rnorm: {rnorm}, mu={mu}, v={v}")
+
         norm_hist.append(rnorm.item())
+
         if rnorm < rtol:
-            return x, theta
-        
+            if log_each_step:
+                return u, theta, norm_hist, res_dict, residual.square().mean().sqrt().cpu(), log_dict
+            else:
+                return u, theta, norm_hist, res_dict, residual.square().mean().sqrt().cpu()
+
         # Compute a proposed step and re-evaluate residual vector
-        D = torch.diag(mu*torch.ones(Hx.shape[0])).to(device=Hx.device)
-        p = torch.linalg.solve((Hx + D), -g)
-        
-        xnew_flatten = x.flatten() + p[:-1]
-        xnew = xnew_flatten.reshape(x.shape)
-        theta_new = theta + p[-1].item()
-        fxnew = f(xnew, theta_new)
-        
+        D = mu * torch.eye(hessian.size(0), device=hessian.device)
+        p = torch.linalg.solve(hessian + D, -g)
+
+        updated_u = u + p[:-1].view(u.shape)
+        updated_theta = theta + p[-1].item()
+        updated_residual, c, Q, R = func_residual(updated_u, updated_theta)
+
         # Compute the gain ratio
-        rho = (torch.norm(fx)**2 - torch.norm(fxnew)**2) / (torch.norm(fx)**2 - torch.norm(fx+torch.matmul(Jx,p))**2)
+        rho = (residual.square().sum() - updated_residual.square().sum()) \
+            / (residual.square().sum() - (residual + jacobian @ p).square().sum()).item()
+
         if rho > 0:  # Success!
-            # print(f"success, theta_new: {theta_new}")
             # Accept new point
-            # print("delete fx, Jx, Hx.")
-            # sys.stdout.flush()
-            # check_cuda_memory()
-            del fx, Jx, Hx
-            x = xnew
-            theta = theta_new
-            fx = fxnew
-            del fxnew, xnew
-            Jx = J(x,theta)
-            Hx = torch.matmul(Jx.T, Jx)
-            # check_cuda_memory()
+            u = updated_u
+            theta = updated_theta
+
+            residual = updated_residual
+            jacobian = func_jacobian(u, theta, updated_residual, c, Q, R)
+            hessian = jacobian.T @ jacobian
+
             # Reset re-scaling parameter, update damping
-            mu = mu*max(1.0/3.0, 1.0-2.0*(rho.item()-1.0)**3)
+            mu = mu * max(1. / 3., 1. - 2. * (rho - 1.) ** 3)
             v = 2.0
         else:
-            # Rescale damping5tb
-            mu = mu*v
-            v = 2*v
+            # Rescale damping
+            mu = mu * v
+            v = 2 * v
 
         if output_steps is not None and k in output_steps:
             print(k)
             sys.stdout.flush()
-            res_dict[k] = {"u": x.cpu(), "theta": theta,
-                           "train_rmse": (fx.cpu()**2).mean().sqrt()}
+            res_dict[k] = {
+                "u": u.cpu(),
+                "theta": theta,
+                "train_rmse": residual.square().mean().sqrt().cpu()
+            }
 
         if log_each_step:
-            log_dict["u"][k] = x.cpu()
+            log_dict["u"][k] = u.cpu()
             log_dict["grad_norm"][k] = rnorm.cpu()
             log_dict["theta"][k] = theta
-            log_dict["training_rmse"][k] = (fx.cpu()**2).mean().sqrt()
+            log_dict["training_rmse"][k] = residual.square().mean().sqrt().cpu()
 
     if log_each_step:
-        return x, theta, norm_hist, res_dict, (fx.cpu()**2).mean().sqrt(), log_dict
+        return u, theta, norm_hist, res_dict, residual.square().mean().sqrt().cpu(), log_dict
     else:
-        return x, theta, norm_hist, res_dict, (fx.cpu()**2).mean().sqrt()
-    
- 
+        return u, theta, norm_hist, res_dict, residual.square().mean().sqrt().cpu()
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+
+    n = 5
+    d = 2
+    m = 3
+
+    u = torch.randn(m, d)
+    x = torch.randn(n, d)
+    y = torch.randn(n)
+
+    inducing_points, lengthscale, norm_hist, _, _ = levenberg_marquardt(u, x, y, 1., theta_penalty=1e-2)
+    print(norm_hist)
---
+++ src/opt/residual.py
@@ -0,0 +1,56 @@
+import torch
+
+import sys
+sys.path.append("../")
+
+from splines import spline_K, Dspline_K
+
+
+def lstsq_residual(u, x, y, theta, phi, sigma=1e-3):
+    m, _ = u.shape
+
+    Kxu = spline_K(x, u, theta, phi)
+
+    A = torch.cat([Kxu, sigma * torch.eye(m, device=u.device)], dim=0)
+    ybar = torch.cat([y, torch.zeros(m, device=u.device)])
+
+    # Q is of size (n + m, m) and R is of size (m, m)
+    Q, R = torch.linalg.qr(A)
+
+    # compute the residual
+    # the parenthesis is crucial (computing Q @ Q.T first is slower)
+    # r = Q @ (Q.T @ ybar) - ybar
+
+    # if both c and r are need, then this is more efficient
+    c = torch.linalg.solve_triangular(R, Q.T @ ybar.unsqueeze(-1), upper=True).squeeze()
+    r = A @ c - ybar
+
+    return r, c, Q, R
+
+
+def lstsq_jacobian(u, x, y, theta, Drho_phi, Dtheta_phi, r, c, Q, R, sigma=1e-3):
+    m, d = u.shape
+    n, d = x.shape
+
+    JA = Dspline_K(x, u, theta, Drho_phi, Dtheta_phi)  # n*md+m
+    JA_ex = torch.cat([JA, torch.zeros(m, m * d + m).to(device=JA.device)], dim=0)
+    z = torch.matmul(JA_ex.T, r)
+    JAtr = torch.zeros(m, m * d + 1).to(device=JA.device)
+
+    # compuet JAc n by md+1
+    Jac_theta = torch.matmul(JA[:, m*d:], c)
+    JAc = torch.cat([JA[:, :m*d], Jac_theta.reshape(-1, 1)], dim=1)
+
+    for j in range(m):
+        J = range(j * d, (j+1)*d)
+        JAc[:, J] *= c[j]
+        JAtr[j, J] = z[J]
+    JAtr[:, -1] = z[m*d:]
+
+    # T1 = torch.linalg.solve_triangular(R.T, JAtr, upper=True)
+    T1 = torch.linalg.solve(R.T, JAtr)
+    T1 -= Q[:n].T @ JAc
+    res = Q @ -T1
+    res[:n] -= JAc
+
+    return res
